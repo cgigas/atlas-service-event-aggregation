c63efba5935259877ea03126f8cbe3a5c5d36538
Coral Gigas <coral.gigas@omitron.com>
2020-10-23 12:48:44 -0600

    updated buffer string

--- event-aggregation-implementation-parent/event-aggregation-service/src/main/java/atlas/event/aggregation/EventAggregationGQLServerApplication.java
+++ event-aggregation-implementation-parent/event-aggregation-service/src/main/java/atlas/event/aggregation/EventAggregationGQLServerApplication.java
@@ -19,7 +19,6 @@ package atlas.event.aggregation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class EventAggregationGQLServerApplication
